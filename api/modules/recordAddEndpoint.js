b0c13444f59c328a41c1ee46b85bd8ee7d794761
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-18 21:26:32 +0530

    Rename api_modules folder to modules folder

---
+++ api/modules/recordAddEndpoint.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    var Record = require('../models/recordModel.js');
+
+    module.exports = function (req, res) {
+        var record = req.query;
+        console.log(record);
+        var newRecord = new Record({
+            slotId: record.slotId,
+            tagNo: record.tagNo,
+            inTime: new Date().getTime().toString(),
+            outTime: '',
+            price: ''
+        });
+
+        newRecord.save(function (err) {
+            if(err) {
+                return res.status(500).json({message: 'Data POST Failed!'});
+            }
+            return res.status(200).json({message: 'Success'});
+        });
+    };
+
+})();
---
+++ api/modules/recordGetEndpoint.js
@@ -0,0 +1,16 @@
+(function () {
+	'use strict';
+
+	var Record = require('../models/recordModel.js');
+
+	module.exports = function (req, res) {
+	    Record.find(function(err, records) {
+	        if (err) {
+	            res.status(500).send(err);
+	        } else {
+	            res.status(200).json(records);
+	        }
+	    });
+	};
+
+})();
