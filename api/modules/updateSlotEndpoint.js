aa1bcd735891ba1f79f368f40ebf2b7ac01bad56
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-31 09:09:20 +0530

    Modify the update slot endpoint

--- api/modules/updateSlotEndpoint.js
+++ api/modules/updateSlotEndpoint.js
@@ -2,6 +2,7 @@
 	'use strict';
 
 	var Slot = require('../models/slotModel.js');
+	var Record = require('../models/recordModel.js');
 
 	module.exports = function (req, res) {
 		var slotData = req.query;
@@ -14,22 +15,23 @@
 
 			if (existingSlot) {
 				// update the availability status of the slot
-				var updatePackage = { 
+				var updatePackage = {
 					availability: slotData.available
 				};
 
 				var updateOptions = {};
-				
-				Slot.update({ slotId: slotData.slotId }, updatePackage, updateOptions, function (err, affected) {
-					if (err) { 
-						return res.status(500).send({ message: 'Error! Updating Slot.' });
-					}
 
-					if (slotData.available) {	
-						updateExistingRecord(res);
-					} else {
-						createNewRecord(slotData, res);						
-					}					
+				Slot.update({ slotId: slotData.slotId }, updatePackage, updateOptions, function (err, affected) {
+					if (err) {
+						return res.status(500).json({ message: 'Error! Updating Slot.' });
+					}
+					//return res.status(200).json({ message: 'Success!. Slot Updated.' });
+					//if (slotData.slotId && !slotData.available) {
+					//	createNewRecord(slotData, res);
+					//} else {
+						updateExistingRecord(slotData, res);
+					//}
+
 				});
 
 			} else {
@@ -43,13 +45,13 @@
 					if (err) {
 						return res.status(500).json({message: 'Error! Creating new Slot'});
 					}
-
+					//return res.status(200).json({ message: 'Success!. New Slot Created.' });
 					createNewRecord(slotData, res);
 				});
 
 			}
 
-		});		
+		});
 	};
 
 	function createNewRecord (slotData, res) {
@@ -64,14 +66,44 @@
 
         newRecord.save(function (err) {
             if(err) {
-                return res.status(500).json({message: 'Error! Creating new Record'});
+                return res.status(500).json({message: 'Error! Creating Billing Record'});
             }
-            return res.status(200).json({message: 'Success'});
+            return res.status(200).json({message: 'Success!. New Billing Record Created.'});
         });
 	}
 
-	function updateExistingRecord (res) {
+	function updateExistingRecord (slotData, res) {
 		console.log('Inside Update Existing Record');
+		Record.find({
+			tagNo: slotData.tagNo
+		}, function (err, docs) {
+			if (err) {
+				return res.status(500).json({message: 'Error! Finding Billing Record'});
+			}
+
+			console.log(docs);
+			var lastRecord = docs.pop();
+			lastRecord.outTime = new Date().getTime().toString();
+			lastRecord.price = calculatePrice(lastRecord.outTime, lastRecord.inTime, 3);
+
+			Record.update({ _id: lastRecord._id }, lastRecord, {}, function (err, affected) {
+				if (err) {
+					return res.status(500).json({message: 'Error! Updating Billing Record'});
+				}
+
+				Slot.update({ slotId: lastRecord.slotId }, { availability: true }, {}, function (err, affected) {
+					if (err) {
+						return res.status(500).json({message: 'Error! Updating Slot'});
+					}
+					return res.status(200).json({ message: 'Success!. Billing Record & Slot Updated.' });
+				});
+
+			});
+		});
+	}
+
+	function calculatePrice (inTime, outTime, basePrice) {
+		return Math.ceil(((Math.abs(inTime) - Math.abs(outTime)) / (1000 * 60)) * basePrice).toString();
 	}
 
 })();
