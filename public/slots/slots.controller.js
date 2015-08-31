471f1092b3fa57266ef9ac1a0c884dad6cb2c657
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-31 10:28:22 +0530

    Modify the slot views html and the controller to display slot information

--- public/slots/slots.controller.js
+++ public/slots/slots.controller.js
@@ -5,7 +5,17 @@
 		.module('iot_app')
 		.controller('SlotsController', slotsController);
 
-	function slotsController ($scope, $http) {
+	function slotsController ($interval, slots, slotResource) {
+		var vm  = this;
 
-	};
+		vm.slots = slots;
+		console.log(slots);
+
+		vm.updateSlots = function () {
+			slotResource.query(function (data) {
+				vm.slots = data;
+			});
+		};
+	}
+
 })();
