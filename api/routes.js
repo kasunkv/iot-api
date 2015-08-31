f4967e3a33ea85dacde0123dfc76ece70298ec9f
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-31 09:14:59 +0530

    Added the endpoint for getting slots

--- api/routes.js
+++ api/routes.js
@@ -5,12 +5,14 @@
 	var recordAddEndpoint = require('./modules/recordAddEndpoint');
 	var recordPostEndpoint = require('./modules/recordPostEndpoint');
 	var updateSlotEndpoint = require('./modules/updateSlotEndpoint');
+	var slotGetEndpoint = require('./modules/slotGetEndpoint');
 
 	module.exports = function (app) {
 		app.get('/api/get', recordGetEndpoint);
 		app.get('/api/add', recordAddEndpoint);
 		app.get('/api/update-slot', updateSlotEndpoint);
 		app.post('/api/post', recordPostEndpoint);
+		app.get('/api/slots', slotGetEndpoint);
 
 		app.get('*', function (req, res) {
 			res.sendFile('index.html', { root: __dirname + '../public/' });
