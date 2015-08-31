31c9a716dfe2ba67c73e1734378b491798c036bb
Kasun Kodagoda <kvkrusl@gmail.com>
2015-08-31 09:18:22 +0530

    Added the code for the slot get endpoint

---
+++ api/modules/slotGetEndpoint.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    var Slot = require('../models/slotModel.js');
+    
+    module.exports = function (req, res) {
+        Slot.find(function(err, records) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.status(200).json(records);
+            }
+        });
+    };
+
+})();
+
